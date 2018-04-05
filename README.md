7b69788a626ee038e122deeefaa62acd47f26f8f
rezaarifandee <rezaarifandee@gmail.com>
2018-04-06 01:10:32 +0700

    Added install script and added installation instruction.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # FastDotFiles
 My linux dotfiles for my fast machines (>2.0 GHz).
 
+# Installation
+Run install.sh to install the config files to your home directoru.
+>./install.sh
+
 # Screenshots
 ![workspace1](screenshots/1.png "Workspace 1")
 ![workspace2](screenshots/2.png "Workspace 2")
---
+++ install.sh
@@ -0,0 +1,17 @@
+mkdir ~/.config
+mkdir ~/.config/i3
+mkdir ~/.config/polybar
+mkdir ~/.config/ranger
+mkdir ~/.config/vis
+
+cp -i .Xresources ~/.Xresources
+cp -i .vimrc ~/.vimrc
+cp -i .xinitrc ~/.xinitrc
+cp -i .bashrc ~/.bashrc
+cp -i .config/i3/config ~/.config/i3/config
+cp -i .config/i3/mpd ~/.config/i3/mpd
+cp -i .config/polybar/config ~/.config/polybar/config
+cp -i .config/polybar/start.sh ~/.config/polybar/start.sh
+cp -i .config/ranger/rc.conf ~/.config/ranger/rc.conf
+cp -i .config/vis/config ~/.config/vis/config
+cp -i -r .config/wallpapers ~/.config/wallpapers
