521adfb2393bd284a6148145ca00c5a4d7d12534
rezaarifandee <rezaarifandee@gmail.com>
2018-04-08 05:51:27 +0700

    +Multi-monitor support

--- .config/polybar/start.sh
+++ .config/polybar/start.sh
@@ -2,4 +2,10 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar bar1
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar1 &
+  done
+else
+  polybar --reload bar1 &
+fi
